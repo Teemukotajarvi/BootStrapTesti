71e0104b396fd0bef6fe4583f43e0173ee60f251
Teemu Kotajärvi <teemu.kotajarvi@stud.pointcollege.fi>
2016-12-21 06:20:40 +0200

    ASP.Net Sovelluksen perus versio.

---
+++ BootStrapWebApp/BootStrapWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootStrapWebApp.Startup))]
+namespace BootStrapWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
